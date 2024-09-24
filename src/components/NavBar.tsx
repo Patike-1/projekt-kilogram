ffcd73476ba27e19d0fff74b96be7b1e0803953e
Patike-1 <patrik69kasanicky@gmail.com>
2024-09-24 12:39:15 +0200

    routing update

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+// src/components/NavBar.tsx
+
+import  Typography  from "@mui/material/Typography";
+import Cointainer from "@mui/material/Container";
+
+export const metadata = { title: " NavBar | Kilogram " };
+
+export default function NavBar() {
+    
+  return (
+    <Cointainer> 
+      <Typography> NavBar </Typography>
+    </Cointainer>
+  );
+}
