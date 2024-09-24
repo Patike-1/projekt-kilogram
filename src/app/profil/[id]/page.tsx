3fdd0b65ea46da2414dcce441332f8bb32ab411c
Patike-1 <patrik69kasanicky@gmail.com>
2024-09-24 12:22:58 +0200

    notes added

---
+++ src/app/profil/[id]/page.tsx
@@ -0,0 +1,15 @@
+// src/app/profil/[id]/page.tsx
+
+import  Typography  from "@mui/material/Typography";
+import Cointainer from "@mui/material/Container";
+
+export const metadata = { title: " Detail profilu | Kilogram " };
+
+export default function ProfileDetail() {
+    
+  return (
+    <Cointainer> 
+      <Typography> Detail profilu  </Typography>
+    </Cointainer>
+  );
+}
---
+++ src/app/profil/page.tsx
@@ -0,0 +1,15 @@
+// src/app/profil/page.tsx
+
+import  Typography  from "@mui/material/Typography";
+import Cointainer from "@mui/material/Container";
+
+export const metadata = { title: " Zoznam profilov | Kilogram " };
+
+export default function ProfileList() {
+    
+  return (
+    <Cointainer> 
+      <Typography> Zoznam profilov </Typography>
+    </Cointainer>
+  );
+}
