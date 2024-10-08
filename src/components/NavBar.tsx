2bb706fcf8947abd6acce2180272dfe93c086fd6
Patike-1 <patrik69kasanicky@gmail.com>
2024-10-08 11:25:22 +0200

    update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,30 @@
 // src/components/NavBar.tsx
+"use client";
 
-import  Typography  from "@mui/material/Typography";
-import Cointainer from "@mui/material/Container";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export const metadata = { title: " NavBar | Kilogram " };
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-export default function NavBar() {
-    
   return (
-    <Cointainer> 
-      <Typography> NavBar </Typography>
-    </Cointainer>
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
   );
 }
